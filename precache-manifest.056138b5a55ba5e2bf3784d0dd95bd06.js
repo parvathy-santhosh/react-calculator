c1ff71b4d1a6444a7d0d7b8ef805dbeff6193a85
Parvathy Santhosh <parsan1998@gmail.com>
2018-11-11 22:36:09 +0530

    Updates

---
+++ precache-manifest.056138b5a55ba5e2bf3784d0dd95bd06.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a2ecf2e68de309a6bd2b",
+    "url": "/react-calculator/static/js/runtime~main.a2ecf2e6.js"
+  },
+  {
+    "revision": "350181fb9dbaf12b9b15",
+    "url": "/react-calculator/static/js/main.350181fb.chunk.js"
+  },
+  {
+    "revision": "0b13b4885e8ccff8ae99",
+    "url": "/react-calculator/static/js/1.0b13b488.chunk.js"
+  },
+  {
+    "revision": "350181fb9dbaf12b9b15",
+    "url": "/react-calculator/static/css/main.0172e27a.chunk.css"
+  },
+  {
+    "revision": "f919c8fd1c0bb88867b57121f12e1a31",
+    "url": "/react-calculator/index.html"
+  }
+];
